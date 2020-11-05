3495b59264ae56ab82f6a0fb50f02620f070c46c
dhill30 <dhill30@uw.edu>
2020-11-05 14:56:35 -0800

    removed contacts

--- GroupChat/app/src/main/java/edu/uw/tcss450/groupchat/ui/contacts/Contact.java
+++ GroupChat/app/src/main/java/edu/uw/tcss450/groupchat/ui/contacts/Contact.java
@@ -1,5 +1,8 @@
 package edu.uw.tcss450.groupchat.ui.contacts;
 
+/**
+ *
+ */
 public class Contact {
 
     private String mUsername;
@@ -8,26 +11,47 @@ public class Contact {
 
     private String mEmail;
 
+    /**
+     * Generic constructor, should NEVER be called.
+     */
     public Contact() {
         mUsername = "Blank";
         mName = "Blank Name";
         mEmail = "test@fakemail.com";
     }
 
+    /**
+     * Contact class constructor, initializes the contact with the passed arguments.
+     * @param username the username of the contact
+     * @param name the name (first and last) of the contact
+     * @param email the email of the contact
+     */
     public Contact(final String username, final String name, final String email) {
         mUsername = username;
         mName = name;
         mEmail = email;
     }
 
+    /**
+     * 
+     * @return
+     */
     public String getUsername() {
         return mUsername;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getName() {
         return mName;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getEmail() {
         return mEmail;
     }
