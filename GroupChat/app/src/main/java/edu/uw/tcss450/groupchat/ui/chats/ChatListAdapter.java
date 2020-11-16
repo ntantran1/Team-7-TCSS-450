cf01469b32c835556b129c21247ac34be2e8345c
Sukhraj Kaur <skaur65@uw.edu>
2020-11-16 14:57:17 -0800

    Create ChatListFragment and RecyclerViewAdapter. Implementation needed.

---
+++ GroupChat/app/src/main/java/edu/uw/tcss450/groupchat/ui/chats/ChatListAdapter.java
@@ -0,0 +1,26 @@
+package edu.uw.tcss450.groupchat.ui.chats;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ChatListAdapter extends RecyclerView.Adapter {
+
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
