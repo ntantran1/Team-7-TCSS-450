39b13e5245e5c925a91cae8d2e9af6bff5be51a8
Sukhraj Kaur <skaur65@uw.edu>
2020-11-16 15:58:58 -0800

    Finish Chat UI

--- GroupChat/app/src/main/java/edu/uw/tcss450/groupchat/ChatActivity.java
+++ GroupChat/app/src/main/java/edu/uw/tcss450/groupchat/ChatActivity.java
@@ -1,14 +1,31 @@
 package edu.uw.tcss450.groupchat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.List;
+
+import edu.uw.tcss450.groupchat.ui.chats.ChatListAdapter;
+import edu.uw.tcss450.groupchat.ui.chats.ChatMessage;
+
 public class ChatActivity extends AppCompatActivity {
 
+    private RecyclerView mChatRecycler;
+    private ChatListAdapter mChatAdapter;
+    public List<ChatMessage> chatList;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        mChatRecycler = (RecyclerView) findViewById(R.id.reyclerview_chat_list);
+
+        //this might need to be changed.
+        mChatAdapter = new ChatListAdapter(this, chatList);
+        mChatRecycler.setLayoutManager(new LinearLayoutManager(this));
     }
 }
