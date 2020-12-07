ad00d0468702c6fa85740a72e576881a837bd7c5
Dylan <dhill30@uw.edu>
2020-12-06 16:11:13 -0800

    add notification bubble to chat rooms

--- GroupChat/app/src/main/java/edu/uw/tcss450/groupchat/ui/chats/ChatRoomRecyclerViewAdapter.java
+++ GroupChat/app/src/main/java/edu/uw/tcss450/groupchat/ui/chats/ChatRoomRecyclerViewAdapter.java
@@ -5,6 +5,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -12,6 +14,7 @@ import java.util.List;
 
 import edu.uw.tcss450.groupchat.R;
 import edu.uw.tcss450.groupchat.databinding.FragmentChatCardBinding;
+import edu.uw.tcss450.groupchat.model.chats.ChatNotificationsViewModel;
 
 /**
  * The class describes how each chat room should look on the home page and manage the list of
@@ -24,13 +27,20 @@ public class ChatRoomRecyclerViewAdapter extends
 
     private List<ChatRoom> mRooms;
 
+    private FragmentActivity mActivity;
+
+    private ChatNotificationsViewModel mNewChatModel;
+
     /**
      * Constructor initialize list of rooms.
      *
      * @param items List of ChatRoom objects visible to the user.
      */
-    public ChatRoomRecyclerViewAdapter(List<ChatRoom> items) {
+    public ChatRoomRecyclerViewAdapter(List<ChatRoom> items,
+                                       FragmentActivity activity) {
         this.mRooms = items;
+        mActivity = activity;
+        mNewChatModel = new ViewModelProvider(mActivity).get(ChatNotificationsViewModel.class);
     }
 
     @NonNull
@@ -83,6 +93,8 @@ public class ChatRoomRecyclerViewAdapter extends
         void setRoom(final ChatRoom room) {
             mRoom = room;
             binding.labelName.setText(room.getName());
+            binding.imageNotification.setVisibility(View.INVISIBLE);
+
             //when someone clicks on a chat, takes to that chat list
             mView.setOnClickListener(view -> {
                 Navigation.findNavController(mView).getGraph().findNode(R.id.chatDisplayFragment).setLabel(mRoom.getName());
@@ -91,7 +103,20 @@ public class ChatRoomRecyclerViewAdapter extends
                                 .actionNavigationChatsToChatDisplayFragment(mRoom));
             });
 
+            mNewChatModel.addMessageCountObserver(mActivity, notifications -> {
+                int count = 0;
+                if (notifications.containsKey(mRoom.getId())) {
+                    count = notifications.get(mRoom.getId());
+                }
 
+                if(count > 0) {
+                    //mew messages
+                    binding.imageNotification.setVisibility(View.VISIBLE);
+                } else {
+                    //remove badge
+                    binding.imageNotification.setVisibility(View.INVISIBLE);
+                }
+            });
         }
     }
 }
