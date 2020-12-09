331420eb842e2a45baab8a03618caf0a5ca92c1e
hieuc <hieu2192012@gmail.com>
2020-12-09 03:07:45 -0800

    added send templated image/gif

---
+++ GroupChat/app/src/main/java/edu/uw/tcss450/groupchat/ui/chats/ChatEditText.java
@@ -0,0 +1,73 @@
+package edu.uw.tcss450.groupchat.ui.chats;
+
+import android.content.ClipDescription;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
+
+import androidx.core.os.BuildCompat;
+import androidx.core.view.inputmethod.EditorInfoCompat;
+import androidx.core.view.inputmethod.InputConnectionCompat;
+import androidx.core.view.inputmethod.InputContentInfoCompat;
+
+public class ChatEditText extends androidx.appcompat.widget.AppCompatEditText {
+
+    private KeyBoardInputCallbackListener keyBoardInputCallbackListener;
+
+    public ChatEditText(Context context) {
+        super(context);
+    }
+
+    public ChatEditText(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+    }
+
+    @Override
+    public InputConnection onCreateInputConnection(EditorInfo editorInfo) {
+        final InputConnection ic = super.onCreateInputConnection(editorInfo);
+        EditorInfoCompat.setContentMimeTypes(editorInfo,
+                new String[]{"image/*"});
+
+        final InputConnectionCompat.OnCommitContentListener callback =
+                new InputConnectionCompat.OnCommitContentListener() {
+                    @Override
+                    public boolean onCommitContent(InputContentInfoCompat inputContentInfo,
+                                                   int flags, Bundle opts) {
+                        // read and display inputContentInfo asynchronously
+                        if (BuildCompat.isAtLeastNMR1() && (flags &
+                                InputConnectionCompat.INPUT_CONTENT_GRANT_READ_URI_PERMISSION) != 0) {
+                            try {
+                                inputContentInfo.requestPermission();
+                            } catch (Exception e) {
+                                return false; // return false if failed
+                            }
+                        }
+                        // read and display inputContentInfo asynchronously.
+                        if (keyBoardInputCallbackListener != null) {
+                            keyBoardInputCallbackListener.onCommitContent(inputContentInfo, flags, opts);
+                        }
+
+                        // call inputContentInfo.releasePermission() as needed.
+
+                        return true;  // return true if succeeded
+                    }
+                };
+        return InputConnectionCompat.createWrapper(ic, editorInfo, callback);
+    }
+
+
+    public interface KeyBoardInputCallbackListener {
+        void onCommitContent(InputContentInfoCompat inputContentInfo,
+                             int flags, Bundle opts);
+    }
+
+    public void setKeyBoardInputCallbackListener(KeyBoardInputCallbackListener keyBoardInputCallbackListener) {
+        this.keyBoardInputCallbackListener = keyBoardInputCallbackListener;
+    }
+
+}
